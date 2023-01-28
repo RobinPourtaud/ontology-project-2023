fb83ffa565fd2865289c90c37528c5e0162019e9
RobinPourtaud <robin@pourtaud.dev>
2023-01-28 11:01:39 +0100

    add pdfToCleanText

--- tools/pdfToCleanText.py
+++ tools/pdfToCleanText.py
@@ -1,4 +1,37 @@
+def pdfToCleanText(pdfPath : str = "rawData/EU.pdf", outputPath : str = "data/EU.txt", pages : list = None) -> str:
+    """Convert pdf to clean text
 
+    Args:
+        pdfPath (str, optional): Path to pdf file. Defaults to "rawData/EU.pdf".
+        outputPath (str, optional): Path to output file. Defaults to "data/EU.txt".
 
-def pdfToCleanText():
-    pass
+    Returns:
+        str: Clean text from pdf
+    """
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.converter import TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfpage import PDFPage
+    from io import StringIO
+
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = open(pdfPath, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close()
+
+    with open(outputPath, "w") as f:
+        f.write(text)
+    return ' '.join(text.split())
