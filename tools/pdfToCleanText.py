22fd7a1b44f96f6295175ad17f59ade6af3aa468
RobinPourtaud <robin@pourtaud.dev>
2023-01-28 11:57:21 +0100

    add PDFcleanToText

--- tools/pdfToCleanText.py
+++ tools/pdfToCleanText.py
@@ -1,11 +1,11 @@
-def pdfToCleanText(pdfPath : str = "rawData/EU.pdf", outputPath : str = "data/EU.txt", pages : list = None, preProcess : bool = True) -> str:
+def pdfToCleanText(pdfPath : str = "rawData/EU.pdf", outputPath : str = "data/corpus.txt", pages : list = None, returnCorpus : bool = True) -> str:
     """Convert pdf to clean text
 
     Args:
         pdfPath (str, optional): Path to pdf file. Defaults to "rawData/EU.pdf".
         outputPath (str, optional): Path to output file. Defaults to "data/EU.txt".
         pages (list, optional): List of pages to convert. Defaults to None.
-        preProcess (bool, optional): Preprocess text. Defaults to True. It will return a set of sentences.
+        returnCorpus (bool, optional): Preprocess text. Defaults to True. It will return a set of sentences.
 
     Returns:
         str: Clean text from pdf
@@ -16,8 +16,8 @@ def pdfToCleanText(pdfPath : str = "rawData/EU.pdf", outputPath : str = "data/EU
     from pdfminer.pdfpage import PDFPage
     from io import StringIO
 
-    if not pages:
-        pagenums = set()
+    if pages == None:
+        pagenums = set(list(range(2, 59)) + list(range(61, 104)))
     else:
         pagenums = set(pages)
 
@@ -34,12 +34,30 @@ def pdfToCleanText(pdfPath : str = "rawData/EU.pdf", outputPath : str = "data/EU
     text = output.getvalue()
     output.close()
 
-    if preProcess:
+    if returnCorpus:
+        # remove \n, \t, \r
+        text = text.replace("\n", "").replace("\t", "").replace("\r", "").replace("\x0c", "").replace("\x0b", "").replace("15698/22 TREE.2.B RB/ek", "")
+        # remove multiple spaces
+        text = " ".join(text.split())
+
         from nltk.tokenize import sent_tokenize
-        text = sent_tokenize(text)
+        from nltk import download
+        try:
+            text = sent_tokenize(text)
+        except Exception as e:
+            download("punkt")
+            text = sent_tokenize(text)
+        # remove small sentences
+        text = [sentence for sentence in text if len(sentence) > 10]
+        # split sentences with spaces
+        text = [sentence.split() for sentence in text]
+
+        # keep only NP 
+        
     else:
         text = text.split()
 
     with open(outputPath, "w") as f:
-        f.write(text)
+        for sentence in text:
+            f.write(" ".join(sentence) + "\n")
     return text
